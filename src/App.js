fbf0a2e123b3d81aaead55a33c024bdf5bb0fe0f
mc2 <themikechase@gmail.com>
2023-12-19 19:26:49 -0500

    darwin 0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./Layout";
+import Login from "./Login";
+import Signup from "./Signup";
+import Profile from "./Profile";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+          <Route path = "/" element = { <Layout></Layout> }>
+            <Route index element = { <Login></Login> }></Route>
+            <Route path = "/signup" element = { <Signup></Signup> } ></Route>
+            <Route path = "/profile" element = { <Profile></Profile> }></Route>
+          </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBY1JESxAdFkLi3lKOpcrVsJQhJ6vbEq6M",
+  authDomain: "darwin-2968b.firebaseapp.com",
+  projectId: "darwin-2968b",
+  storageBucket: "darwin-2968b.appspot.com",
+  messagingSenderId: "857906183884",
+  appId: "1:857906183884:web:2e8ad204b2920fade2cc69",
+  measurementId: "G-4JQTTM2GM6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Get auth object
+const auth = getAuth(app);
+
+export { auth }; // Export auth object
+export default app; // Export the entire Firebase app if needed
