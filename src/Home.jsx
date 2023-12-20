e987d4a22fbcb2c52c8570bf137f415f7023bdfa
mc2 <themikechase@gmail.com>
2023-12-20 17:06:16 -0500

    adding home and user auth

---
+++ src/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const Home = () => {
+  // Your Home component logic here
+
+  return (
+    <div className="container">
+      {/* Sidebar */}
+      <div className="sidebar">
+      </div>
+
+      {/* Main content */}
+      <div className="main-content">
+        {/* Your existing content goes here */}
+        Build your first Darwin App!
+      </div>
+    </div>
+  );
+};
+
+export default Home;
